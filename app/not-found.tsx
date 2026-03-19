0faea7d037553accdde32b458cadf5462e8f3fb9
Xzonn <Xzonn@outlook.com>
2026-03-19 18:46:31 +0800

    feat: Update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,55 @@
+"use client";
+
+import Head from "next/head";
+import { Fragment, useEffect } from "react";
+
 import { PokemonIcon } from "@/components";
 import { PokemonDataByName } from "@/data";
-import { Link } from "@/utils";
+import { DEFAULT_TITLE, Link } from "@/utils";
 
-const NotFoundPage = () => (
-  <div
-    key="not-found"
-    className="text-center py-20"
-  >
-    <div className="bg-red-100 rounded-full w-24 h-24 flex items-center justify-center mx-auto mb-6">
-      <PokemonIcon pokemon={PokemonDataByName["梦幻"]} />
-    </div>
-    <h1>页面未找到</h1>
-    <p className="text-xl text-gray-600 mb-8">您访问的页面不存在或已被删除。</p>
-    <Link
-      href="/"
-      className="px-8 py-3 rounded-full font-semibold border-2 border-primary bg-white hover:bg-secondary transition-colors duration-300"
-    >
-      返回首页
-    </Link>
-  </div>
-);
+const NotFoundPage = () => {
+  useEffect(() => {
+    document.title = `页面未找到 - ${DEFAULT_TITLE}`;
+
+    document.querySelector(".giscus")?.classList.add("hidden");
+
+    return () => {
+      document.querySelector(".giscus")?.classList.remove("hidden");
+    };
+  }, []);
+
+  return (
+    <Fragment key="not-found">
+      <Head>
+        <title>页面未找到 - {DEFAULT_TITLE}</title>
+      </Head>
+      <section
+        key="not-found"
+        className="not-found-block"
+      >
+        <div className="not-found-icon">
+          <PokemonIcon pokemon={PokemonDataByName["梦幻"]} />
+        </div>
+        <h1>页面未找到</h1>
+        <p className="text-xl text-gray-600 mb-8">您访问的页面不存在或已被删除。</p>
+        <div className="not-found-actions">
+          <Link
+            href="/"
+            className="not-found-button"
+          >
+            返回首页
+          </Link>
+          <Link
+            href="/"
+            onClick={() => window.history.back()}
+            className="not-found-button"
+          >
+            返回上一页
+          </Link>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
 export default NotFoundPage;
